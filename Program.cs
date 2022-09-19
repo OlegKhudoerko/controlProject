b0da160cf41a98dd5c8fb95934244be2bcf7a96a
OlegKhudoerko <292oleg292@mail.ru>
2022-09-20 02:03:44 +0300

    add InputArray

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿
+string[] array = InputArray()!.Split();
+string InputArray()
+{
+    Console.Write("\nВведите через ПРОБЕЛ несколько слов,символов или чисел  => ");
+    string? getArray = Console.ReadLine();
+    return getArray!;
+}
