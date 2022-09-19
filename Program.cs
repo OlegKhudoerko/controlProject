5603d06c6c6e3f8d1c8fdcd2790de5e7a1821460
OlegKhudoerko <292oleg292@mail.ru>
2022-09-20 02:08:58 +0300

    add method CountMaxLength

--- Program.cs
+++ Program.cs
@@ -1,5 +1,18 @@
-﻿
+﻿int maxLenght = 3;
 string[] array = InputArray()!.Split();
+int CountMaxLength()
+{
+    int j = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= maxLenght)
+        {
+            j++;
+        }
+    }
+    return j;
+}
+
 string InputArray()
 {
     Console.Write("\nВведите через ПРОБЕЛ несколько слов,символов или чисел  => ");
